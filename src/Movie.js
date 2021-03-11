03d3382f77988b9c1a2f87160389f22bde1f8367
wonsik465 <wonsik465@naver.com>
2021-03-12 01:16:17 +0900

    03-12

--- src/Movie.js
+++ src/Movie.js
@@ -9,9 +9,11 @@ function Movie({ year, title, summary, poster, genres }) {
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <ul className="genres">
-          {genres.map((genre) => (
-            <li className="genres__genre">{genre}</li>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
           ))}
         </ul>
         <p className="movie__summary">{summary}</p>
